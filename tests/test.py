c563d5ee6a3d0c0788a1b58a712b4d05e7149080
Ernest <iattempt.net@gmail.com>
2018-03-08 01:37:04 +0800

    Add some tests

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,17 @@
 import time
 import sys
 import os
+import string
+import random
+import itertools
 
 sys.path.append(os.sep.join([os.path.dirname(os.path.realpath(__file__)),
                              '..',
                              'file_checker'
                              ]))
 import util # noqa
+from file_checker.file_checker import FileChecker # noqa
+from file_checker.enums import FORMATS # noqa
 
 
 class TestUtil():
@@ -57,3 +62,47 @@ class TestUtil():
             get_first_word('l' * 256)
             get_first_word('U' * 256)
         assert time.time() - start < 5
+
+
+class TestFileChecker():
+    def _gen_random_alphabet_string(self, length=30):
+        s = ''
+        for i in range(0, length):
+            s += random.choice(string.ascii_letters)
+
+        return s
+
+    def _gen_all_possible_pair(self, iterable, beg=0, end=-1):
+        """
+        :param iterable:
+        :type iterable: list, str or iterable types
+        :return: tuple -
+        """
+        if end == -1:
+            end = len(iterable)
+        res = []
+        for i in range(beg, end):
+            for comb in itertools.combinations(iterable, i + 1):
+                res += itertools.permutations(comb)
+        return res
+
+    def test_convert_sep(self):
+        fc = FileChecker()
+
+        # boundary
+        for sep in self._gen_all_possible_pair(FORMATS['sep']):
+            assert '' == fc.convert_sep('', list(sep))
+        for s in ['_', '_a', 'a_', 'a_a', '-', '-a', 'a-', 'a-a']:
+            assert s == fc.convert_sep(s, [])
+
+        # dash_to_underscore
+        assert '_' == fc.convert_sep('-', ['dash_to_underscore'])
+        assert '_a' == fc.convert_sep('-a', ['dash_to_underscore'])
+        assert 'a_' == fc.convert_sep('a-', ['dash_to_underscore'])
+        assert 'a_a' == fc.convert_sep('a-a', ['dash_to_underscore'])
+
+        # underscore_to_dash
+        assert '-' == fc.convert_sep('_', ['underscore_to_dash'])
+        assert '-a' == fc.convert_sep('_a', ['underscore_to_dash'])
+        assert 'a-' == fc.convert_sep('a_', ['underscore_to_dash'])
+        assert 'a-a' == fc.convert_sep('a_a', ['underscore_to_dash'])
