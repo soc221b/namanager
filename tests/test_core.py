398b69b8efb32aea02b8ba8d818501855b0dc007
Ernest <iattempt.net@gmail.com>
2018-03-27 22:27:01 +0800

    Change dump format

--- tests/test_core.py
+++ tests/test_core.py
@@ -608,8 +608,8 @@ class TestNamanager():
         fc.check_file(etc['CHECK_DIRS'][0])
 
         for f in fc.get_dict():
-            expect = f['filename']['expect']
-            actual = f['filename']['actual']
+            expect = f['expect']
+            actual = f['actual']
             helper.append_to_error_if_not_expect_with_msg(
                 errors, len(expect) == len(actual), (
                     "expect: {0}\nactual: {1}\nin: {2}".format(
