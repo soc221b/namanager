320508ade6290473d73b11ae1540b7840d40ecf9
Ernest <iattempt.net@gmail.com>
2018-04-05 14:54:34 +0800

    Generate testfiles to specified directory

--- tests/test_archieve_manager.py
+++ tests/test_archieve_manager.py
@@ -9,11 +9,15 @@ class TestArchieveManager():
     def __init__(self):
         self.TMPFILE_PREFIX = 'test_am_'
         self.RENAME_SUFFIX = '_*&^%$'
+        self.TMP_ROOT = os.sep.join(
+            [os.path.realpath(os.path.dirname(__file__)), 'test_root'])
+        if not os.path.exists(self.TMP_ROOT):
+            os.mkdir(self.TMP_ROOT)
 
     def test_rename(self):
         # test files/directories both mixed
         am = ArchieveManager()
-        paths = helper.mkdtemps_recur(1, 1, filecount=1,
+        paths = helper.mkdtemps_recur(1, 1, filecount=1, root=self.TMP_ROOT,
                                       prefix=self.TMPFILE_PREFIX)
         path_pairs = self._gen_src_dst_rename_pairs(paths)
         renamed_path_pairs = self._gen_src_dst_renamed_pairs(paths)
@@ -25,11 +29,10 @@ class TestArchieveManager():
             self._test_rename_after(renamed_path_pairs)
             assert [] == am.rename(revert_path_pairs)
 
-        helper.rm_paths([pair[0] for pair in pairs])
-
     def test_rename_file(self):
         am = ArchieveManager()
-        files = helper.mktemps(2, prefix=self.TMPFILE_PREFIX)
+        files = helper.mktemps(2, prefix=self.TMPFILE_PREFIX,
+                               root=self.TMP_ROOT)
         file_pairs = self._gen_src_dst_rename_pairs(files)
         revert_file_pairs = self._gen_src_dst_revert_pairs(files)
 
@@ -41,11 +44,10 @@ class TestArchieveManager():
             self._test_rename_after(pairs)
             assert [] == am.rename(revert_file_pairs)
 
-        helper.rm_paths([pair[0] for pair in file_pairs])
-
     def test_rename_dir(self):
         am = ArchieveManager()
-        dirs = helper.mkdtemps_recur(2, 1, prefix=self.TMPFILE_PREFIX)
+        dirs = helper.mkdtemps_recur(2, 1, prefix=self.TMPFILE_PREFIX,
+                                     root=self.TMP_ROOT)
         dir_pairs = self._gen_src_dst_rename_pairs(dirs)
         renamed_dir_pairs = self._gen_src_dst_renamed_pairs(dirs)
         revert_dir_pairs = self._gen_src_dst_revert_pairs(dirs)
@@ -55,8 +57,6 @@ class TestArchieveManager():
             assert [] == am.rename(perm_pairs)
             self._test_rename_after(renamed_dir_pairs)
             assert [] == am.rename(revert_dir_pairs)
-
-        helper.rm_paths([pair[0] for pair in dir_pairs])
 
     def _test_rename_before(self, pairs):  # pragma: no cover
         # haven't been tested
@@ -92,7 +92,7 @@ class TestArchieveManager():
 
     def test_gen_revert_path_pairs(self):
         am = ArchieveManager()
-        paths = helper.mkdtemps_recur(1, 2, file_count=1,
+        paths = helper.mkdtemps_recur(1, 2, file_count=1, root=self.TMP_ROOT,
                                       prefix=self.TMPFILE_PREFIX)
         rename_path_pairs = self._gen_src_dst_rename_pairs(paths)
         expt_revert_path_pairs = self._gen_src_dst_revert_pairs(paths)
@@ -116,14 +116,14 @@ class TestArchieveManager():
                         json.dumps(actl_revert_path_pairs,
                                    indent=4, sort_keys=True)))
 
-        helper.rm_paths([pair[0] for pair in rename_path_pairs])
-
     def test_separate_file_dir_from_path_pair(self):
         am = ArchieveManager()
         errors = []
-        dirs = helper.mkdtemps_recur(2, 1, prefix=self.TMPFILE_PREFIX)
-        files = (helper.mktemps(1, prefix=self.TMPFILE_PREFIX) +
-                 helper.mktemps(1, root=dirs[0], prefix=self.TMPFILE_PREFIX))
+        dirs = helper.mkdtemps_recur(2, 1, prefix=self.TMPFILE_PREFIX,
+                                     root=self.TMP_ROOT)
+        files = (
+            helper.mktemps(1, prefix=self.TMPFILE_PREFIX, root=self.TMP_ROOT) +
+            helper.mktemps(1, root=dirs[0], prefix=self.TMPFILE_PREFIX))
         dir_pairs = self._gen_src_dst_rename_pairs(dirs)
         file_pairs = self._gen_src_dst_rename_pairs(files)
         path_pairs = file_pairs + dir_pairs
@@ -143,14 +143,13 @@ class TestArchieveManager():
 
             assert errors == [], Exception(helper.get_error_string(errors))
 
-        helper.rm_paths([pair[0] for pair in path_pairs])
-
     def test_sort_path_pair(self):
         # only tests for directories
         am = ArchieveManager()
         errors = []
         # create some temp directories under different hierarchy
-        expt_hrchy1_paths = helper.mkdtemps(1, prefix=self.TMPFILE_PREFIX)
+        expt_hrchy1_paths = helper.mkdtemps(1, prefix=self.TMPFILE_PREFIX,
+                                            root=self.TMP_ROOT)
         expt_hrchy2_paths = (
             helper.mkdtemps(2, root=expt_hrchy1_paths[0],
                             prefix=self.TMPFILE_PREFIX) +
@@ -190,10 +189,6 @@ class TestArchieveManager():
                         json.dumps(actl, indent=4, sort_keys=True)))
 
             assert errors == [], Exception(helper.get_error_string(errors))
-
-        helper.rm_paths(expt_hrchy1_paths +
-                        expt_hrchy2_paths +
-                        expt_hrchy3_paths)
 
     def _gen_src_dst_rename_pairs(self, path):  # pragma: no cover
         # haven't been tested
