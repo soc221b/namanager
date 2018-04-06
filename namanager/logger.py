2f04bc50c605254cba275a8cedef8b1b5da983f5
Ernest <iattempt.net@gmail.com>
2018-04-06 18:41:38 +0800

    Update

--- namanager/logger.py
+++ namanager/logger.py
@@ -8,7 +8,7 @@ def logger():
     # inspect.stack(1)[2]: caller infomations.
     caller_filename = inspect.stack(1)[2][1]
     if caller_filename not in _logger_instances:
-        caller = caller_filename.split(os.sep)[-1].split(r'.')[0]
+        caller = caller_filename.split(os.sep)[-1]
         _logger_instances[caller_filename] = logging.getLogger(caller)
     return _logger_instances[caller_filename]
 
