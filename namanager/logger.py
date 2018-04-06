a3d53310877f6e708e65ca429bc5053c3011d4ab
Ernest <iattempt.net@gmail.com>
2018-04-06 16:19:26 +0800

    Init logger

---
+++ namanager/logger.py
@@ -0,0 +1,17 @@
+import inspect
+import logging
+import os
+
+
+def _logger():
+    global _logger_instances
+    # inspect.stack(1)[2]: caller infomations.
+    caller_filename = inspect.stack(1)[2][1]
+    if caller_filename not in _logger_instances:
+        caller = caller_filename.split(os.sep)[-1].split(r'.')[0]
+        _logger_instances[caller_filename] = logging.getLogger(caller)
+    return _logger_instances[caller_filename]
+
+
+_logger_instances = {}
+logger = _logger()
